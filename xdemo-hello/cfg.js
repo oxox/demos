c31f34e0393eb1841dbc780e44e9772880500d70
Levin <mamboer@live.com>
2013-11-22 13:08:05 +0800

    add xdemo-hello

---
+++ xdemo-hello/cfg.js
@@ -0,0 +1,18 @@
+{
+    /* demo名称 */
+    "name": "XDEMO-Helloword",
+    /* demo英文编号 */
+    "ename": "ecd.yixun.xdemohello",
+    /* demo描述 */
+    "descrition": "XDEMO测试演示脚本"
+    /* demo版本 */
+    "version": "1.0.0",
+    /* demo演示的页面（正则表达式） */
+    "matches": ["http://*.yixun.com/*"],
+    /* 注入到页面的css*/
+    "css": ["http://oxox.io/demos/xdemo-hello/style.css"],
+    /* 注入到页面的js */
+    "js": ["http://oxox.io/demos/xdemo-hello/app.js"],
+    /* demo作者 */
+    "author": "levinhuang"
+}
